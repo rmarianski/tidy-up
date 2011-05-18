e9f9f877c900da5c947df8a854f18ff7b989b14d
Robert Marianski <rob@marianski.com>
2011-05-18 00:59:58 -0400

    add url to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject rmarianski/tidy-up "0.0.1"
   :description "ring middleware to clean up html output"
+  :url "http://github.com/rmarianski/tidy-up"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [jtidy "4aug2000r7-dev"]]
   :dev-dependencies [[swank-clojure "1.3.0"]
