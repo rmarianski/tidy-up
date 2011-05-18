fdc192b1b01ac31170f3837ff6c425a854dcc343
Robert Marianski <rob@marianski.com>
2011-05-18 00:57:33 -0400

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject tidy-up "0.1"
+(defproject rmarianski/tidy-up "0.0.1"
   :description "ring middleware to clean up html output"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [jtidy "4aug2000r7-dev"]])
+                 [jtidy "4aug2000r7-dev"]]
+  :dev-dependencies [[swank-clojure "1.3.0"]
+                     [lein-clojars "0.6.0"]])
