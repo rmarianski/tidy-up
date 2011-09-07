305d0033f0b4f94b79bfdc7ffae26cb7e33a0924
Robert Marianski <rob@marianski.com>
2011-09-06 21:48:36 -0400

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rmarianski/tidy-up "0.0.1"
+(defproject rmarianski/tidy-up "0.0.2"
   :description "ring middleware to format html output"
   :url "http://github.com/rmarianski/tidy-up"
   :dependencies [[jtidy "4aug2000r7-dev"]])
